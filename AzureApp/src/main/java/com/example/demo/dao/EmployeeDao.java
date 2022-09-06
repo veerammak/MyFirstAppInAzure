d25f539685cd63afafe71b0dcc00a7f6ca471e7c
veerammak <78897908+veerammak@users.noreply.github.com>
2022-09-06 16:10:14 +0530

    Deleted azureapp directory

--- AzureApp/src/main/java/com/example/demo/dao/EmployeeDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Employee;
-
-public interface EmployeeDao extends JpaRepository<Employee, Integer>{
-
-}
